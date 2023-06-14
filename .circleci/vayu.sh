aef21e8d1c8094ab58a1ad8d5e301c78bca5b85a
missgoin <mister.goindo@gmail.com>
2023-06-14 18:58:40 +0700

    f

--- .circleci/vayu.sh
+++ .circleci/vayu.sh
@@ -48,7 +48,7 @@ FINAL_ZIP_ALIAS=Karenulvay-${TANGGAL}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=neutron
+COMPILER=azure
 
 ##----------------------------------------------------------##
 # Specify Linker
